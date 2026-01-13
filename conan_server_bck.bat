e3718401eb50b2b8f0153425c720867ab7381fa0
Sergio Muñoz <58624086+VOLKERTH@users.noreply.github.com>
2026-01-13 22:44:55 +0100

    New repo

---
+++ conan_server_bck.bat
@@ -0,0 +1,9 @@
+#Copia local
+xcopy /e /c /I "\\IPORIGEN\server\steamcmd\conan_exiles\ConanSandbox\Saved" "D:\conan_bck\Backups Saved\Saved_%date:~-10,2%_%date:~-7,2%_%date:~-4,4%_%time:~0,2%_%time:~3,2%_%time:~6,2%"
+
+
+#Copia en Google Drive
+xcopy /e /c /I "\\IPORIGEN\server\steamcmd\conan_exiles\ConanSandbox\Saved" "G:\Mi unidad\Carpetas de poco uso\Backup\Conan Exiles Server\Saved_%date:~-10,2%_%date:~-7,2%_%date:~-4,4%_%time:~0,2%_%time:~3,2%_%time:~6,2%"
+
+
+
